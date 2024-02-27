4378dce78ebc10703869edec633ea27840ec8b65
Jabrecia Washington <jabwashi2@gmail.com>
2024-02-27 18:11:04 -0500

    made files for components

---
+++ code/project-mini.js
@@ -0,0 +1,4 @@
+// the mini project tab that shows up on the main page; has an image and a title, also a click for more button
+
+const template = document.createElement("template");
+    template.innerHTML = ``;
---
+++ code/project.js
@@ -0,0 +1,4 @@
+// the popup window component that shows up when a project tab is clicked on; has a title, description, and demo video (upload to youtube, make unlisted)
+
+const template = document.createElement("template");
+    template.innerHTML = ``;
