1dce9f0963048ee12e5e1a0a21bd28a13d26e69e
Jabrecia Washington <jabwashi2@gmail.com>
2025-01-19 18:23:52 -0500

    adding updated project files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './styles/App.css';
+import { Navbar } from './components/Navbar.js';
+import ProjectCategoriesPage from './pages/ProjectCategoriesPage.js';
+import { AboutPage } from './pages/AboutPage.js';
+import { ContactPage } from './pages/ContactPage.js';
+import dataJSON from './content.json'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1 id="intro">Welcome To Jabrecia Washington's Portfolio</h1>
+      <ProjectCategoriesPage props={dataJSON}/>
+      <AboutPage />
+      <ContactPage />
+    </div>
+  );
+}
+
+export default App;
