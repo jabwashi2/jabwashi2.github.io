6864d619b45518d15c6c1ac34bc9f4c2dee08bcc
Jabrecia Washington <jabwashi2@gmail.com>
2025-01-19 19:13:44 -0500

    restoring old page

---
+++ code/main.js
@@ -0,0 +1,14 @@
+/* for creating/showing project details components:
+* - each object has isCreated bool (starts false)
+* if false, create component
+* x/close button hides component
+* if created, show again
+*/
+
+// project mini button onclick function: creates detail component or shows component
+function showDetails(projectName) {
+    // look through objects to find mathing title with projectName
+    // if found, see if isCreated == true
+    // if not true, create component
+    // if true, find component and set visibility to show
+}
