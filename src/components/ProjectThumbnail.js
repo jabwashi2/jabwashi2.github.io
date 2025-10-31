478d245715911be3f483c78466217445a91e9bb5
Jabrecia Washington <jabwashi2@gmail.com>
2025-10-31 17:03:50 -0400

    media query update for min screen width 300px

--- src/components/ProjectThumbnail.js
+++ src/components/ProjectThumbnail.js
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import { motion } from "motion/react";
 
 export function ProjectThumbnail(props) {
-    // let project = props.projects.find(project => project.title === props.title);
     return (
         <Link to={ props.category === "web" ? "/Web-Projects/" + props.id : props.category === "uiux" ? "/UIUX-Projects/" + props.id : "/DirectX11-Projects/" + props.id} class="proj-thumb-link">
             <motion.div whileHover={{ scale: 1.1, transition: { duration: 0.2 }}} transition={{ duration: 0.3 }} class="project-thumbnail" id={props.title}>
